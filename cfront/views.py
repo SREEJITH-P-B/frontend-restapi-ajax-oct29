9bfa6a9a6e21e6d1fda1b41ac71ec93bd0f53a93
sreejith.b <sreejith.b@qburst.com>
2020-10-29 18:02:49 +0530

    completed basic functionalities

---
+++ cfront/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+def home(request):
+	return render(request,'cfront/index.html')
+def selectone(request):
+	return render(request,'cfront/showone.html')
+def delete(request):
+	return render(request,'cfront/delete.html')
+def insert(request):
+	return render(request,'cfront/insert.html')
+def update(request):
+	return render(request,'cfront/update.html')
+
+
+
+# Create your views here.
